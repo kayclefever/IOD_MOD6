4d7c38a3a04dc09ca29a161b06ee1a90f3ae23b2
kayclefever <kayclefever3342@gmail.com>
2024-11-21 20:19:43 -0800

    react practice

--- ReactPractice/src/components/Complex Comment/ComplexComment.jsx
+++ ReactPractice/src/components/Complex Comment/ComplexComment.jsx
@@ -28,3 +28,23 @@ function ComplexComment(props) {
 } // save in a new file ComplexComment.jsx, then export it and import into App.jsx
 
 export default ComplexComment;
+
+// <ComplexComment author={comment.author} date={comment.date} text={comment.text} /> add to App.jsx this is the code Carlos was typing out in class
+
+//const comment = {
+//date: new Date(),
+//text: "I hope you enjoy learning React!",
+//author: {
+//name: 'Hello Kitty',
+//avatarUrl: 'https://placekitten.com/g/64/64',
+//} add this code as well
+// object storing comment data - passed as props
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    // author is also an object
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64",
+  },
+};
