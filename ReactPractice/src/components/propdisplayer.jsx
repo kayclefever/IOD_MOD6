9f289c9100722721bde877c6c3bd0d25aba4fcbb
kayclefever <kayclefever3342@gmail.com>
2024-11-21 20:39:44 -0800

    react practice

--- ReactPractice/src/components/propdisplayer.jsx
+++ ReactPractice/src/components/propdisplayer.jsx
@@ -1,4 +1,4 @@
-export default function PropDisplayer(props, children) {
+function PropDisplayer(props, children) {
   const stringProps = JSON.stringify(props);
 
   return (
@@ -9,3 +9,4 @@ export default function PropDisplayer(props, children) {
     </div>
   );
 }
+export default PropDisplayer;
